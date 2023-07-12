acd0f40b467dcd2e8b3ed18fda4c9050282aea32
yalexaner <mail@yalexaner.ru>
2023-07-12 14:34:10 +0500

    replace solution for task 274. H-Index with one-liner

--- src/main/kotlin/HIndex.kt
+++ src/main/kotlin/HIndex.kt
@@ -1,12 +1,5 @@
 import kotlin.math.max
 
 fun hIndex(citations: IntArray): Int {
-    citations.sortedDescending().forEachIndexed { index, value ->
-        when {
-            value == 0 -> return index
-            index + 1 >= value -> return max(value, index)
-        }
-    }
-
-    return citations.size
+    return citations.sortedDescending().withIndex().find { it.index + 1 >= it.value }?.let { max(it.index, it.value) } ?: citations.size
 }
