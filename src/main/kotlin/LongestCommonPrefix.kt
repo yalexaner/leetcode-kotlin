c94879ce86b7839457365ca1e93f5971a4acb740
yalexaner <mail@yalexaner.ru>
2023-07-17 12:44:06 +0500

    update solution for 14. Longest Common Prefix

--- src/main/kotlin/LongestCommonPrefix.kt
+++ src/main/kotlin/LongestCommonPrefix.kt
@@ -1,25 +1,9 @@
-import kotlin.math.min
-
 fun longestCommonPrefix(words: Array<String>): String {
-    var prefix = words[0]
-    for (i in 1 until words.size) {
-        prefix = computeCommonPrefix(prefix, words[i])
-        if (prefix.isEmpty()) {
-            break
-        }
+    var res = ""
+    for (ch in words[0]) {
+        val c = res + ch
+        if (!words.all {it.startsWith(c)}) break
+        res += ch
     }
-
-    return prefix
-}
-
-private fun computeCommonPrefix(first: String, second: String): String {
-    return buildString {
-        for (i in 0 until min(first.length, second.length)) {
-            if (first[i] == second[i]) {
-                append(first[i])
-            } else {
-                break
-            }
-        }
-    }
+    return res
 }
